17f52cf27d06e346d6edbfb702d0cb842b92d21b
Joe Totes <59018247+Totes5706@users.noreply.github.com>
2022-03-29 13:29:17 -0400

    Update start-mainnet-node.sh

--- start-mainnet-node.sh
+++ start-mainnet-node.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo wget -O /mainnet/mainnet-config.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-config.json
-sudo wget -O /mainnet/mainnet-byron-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-byron-genesis.json
-sudo wget -O /mainnet/mainnet-shelley-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-shelley-genesis.json
-sudo wget -O /mainnet/mainnet-alonzo-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-alonzo-genesis.json
-sudo wget -O /mainnet/mainnet-topology.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-topology.json
-
 cardano-node run \
     --config mainnet/mainnet-config.json \
     --topology mainnet/mainnet-topology.json \
