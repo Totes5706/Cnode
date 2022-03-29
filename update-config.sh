ae757e34091d7fb6e6ee2fe7d8d7e80fb076ac5a
Joe Totes <59018247+Totes5706@users.noreply.github.com>
2022-03-29 12:49:43 -0400

    Update update-config.sh

--- update-config.sh
+++ update-config.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 
-
-# mainnet config files
+# update mainnet config files
 sudo wget -O /mainnet/mainnet-config.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-config.json
 sudo wget -O /mainnet/mainnet-byron-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-byron-genesis.json
 sudo wget -O /mainnet/mainnet-shelley-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-shelley-genesis.json
@@ -8,7 +8,7 @@ sudo wget -O /mainnet/mainnet-alonzo-genesis.json https://hydra.iohk.io/job/Card
 sudo wget -O /mainnet/mainnet-topology.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-topology.json
 
 
-# testnet config files
+# update testnet config files
 sudo wget -O /testnet/testnet-config.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-config.json
 sudo wget -O /testnet/testnet-byron-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-byron-genesis.json
 sudo wget -O /testnet/testnet-shelley-genesis.json https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-shelley-genesis.json
