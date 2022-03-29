94f870eff8712210f96a615683d500b48bfb0863
Joe Totes <59018247+Totes5706@users.noreply.github.com>
2022-03-29 11:31:55 -0400

    Update start-testnet-node.sh

--- start-testnet-node.sh
+++ start-testnet-node.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+wget -P /testnet/ https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-config.json
+wget -P /testnet/ https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-byron-genesis.json
+wget -P /testnet/ https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-shelley-genesis.json
+wget -P /testnet/ https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-alonzo-genesis.json
+wget -P /testnet/ https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-topology.json
+
 cardano-node run \
     --config testnet/testnet-config.json \
     --topology testnet/testnet-topology.json \
